5832ebcdebb8b31f27664f8f1f57cd30a9ec4625
MigrainPanda <nicktanaka36@gmail.com>
2024-06-22 20:42:08 -0700

    basic app setup

---
+++ application/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+db = SQLAlchemy()
+engine = create_engine("sqlite:///instance/project.db")
+
+from application.routes.auth import auth_bp
+from application.routes.users import users_bp
+from application.routes.chat import chat_bp
+
+from .models import User, ChatLog, Message
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(users_bp)
+    app.register_blueprint(chat_bp)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
+    
+    return app
+
+# run app
+# python -m application
---
+++ application/__main__.py
@@ -0,0 +1,19 @@
+from flask import request, url_for, render_template, redirect
+from application import create_app
+
+app = create_app()
+
+@app.route("/", methods=['GET', 'POST'])
+def home_page():
+    if request.method == 'GET':
+        return render_template("index.html")
+    
+    if request.method == 'POST':
+        match request.form['home_button']:
+            case 'login': return redirect(url_for('auth.login'))
+            case 'users': return redirect(url_for('users.users'))
+            case 'chat': return redirect(url_for('chat.chat'))
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
