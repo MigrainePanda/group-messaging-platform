ce2777ddd967ebc692e12b9a8b57fab676ba9432
MigrainPanda <nicktanaka36@gmail.com>
2024-07-06 02:38:08 -0700

    implemented chatroom connection, authentication, and message sending

--- application/events.py
+++ application/events.py
@@ -1,26 +1,21 @@
 from flask import request
-from .routes.jwt import is_valid_jwt
+from application.jwt import is_valid_jwt
 from flask_socketio import SocketIO, ConnectionRefusedError
 
 socketio = SocketIO()
 
-@socketio.on("connect")
+@socketio.on('connect')
 def handle_connect():
-    print("handle connect")
+    if is_valid_jwt(request.headers.get('user-auth')):
+        print("client connected")
+    else:
+        raise ConnectionRefusedError('unauthorized')
     
-@socketio.on("user_join")
-def handle_user_join(username):
-    print(f"{username} connected to the chat")
-    
-@socketio.on("message")
+@socketio.on('message')
 def handle_message(data):
     print("message received " + data)
+    socketio.emit('recieve_message', {'user': "meow", 'message': "meow"})
     
-    
-# @socketio.on("connect")
-# def handle_connect(auth):
-#     # print(auth)
-#     if "jwt" in auth and is_valid_jwt(auth["jwt"]):
-#         print("client connected")
-#     else:
-#         raise ConnectionRefusedError("unauthorized")
+@socketio.on('receive_message')
+def handle_receieve_message(data):
+    print(data)
--- application/routes/chat.py
+++ application/routes/chat.py
@@ -8,9 +8,12 @@ from sqlalchemy import select
 
 chat_bp = Blueprint("chat", __name__, url_prefix="/chat")
 
-@chat_bp.route("/room/<int:room_id>", methods=["POST"])
+@chat_bp.route("/room/<int:room_id>", methods=["GET"])
 def chat(room_id):
-    return render_template("chat.html", room_id=room_id)
+    with Session(engine) as session, session.begin():
+        chatlog = session.execute(select(ChatLog).filter_by(id=room_id)).scalar()
+        chatlog = chatlog.__dict__
+    return render_template("chat.html", chatlog=chatlog)
 
 @chat_bp.route("/list", methods=["GET", "POST"])
 def chat_list():
@@ -24,20 +27,21 @@ def chat_list():
     if request.method == "POST":
         match request.form['chat_button']:
             case 'new_submit': 
-                with Session(engine) as session, session.begin():
-                    chatlog = ChatLog(
-                        name = request.form['chatname'],
-                        messages = []
-                    )
-                    session.add(chatlog)
-                    session.commit()
+                if len(request.form['chatname']) >= 3:
+                    with Session(engine) as session, session.begin():
+                        chatlog = ChatLog(
+                            name = request.form['chatname'],
+                            messages = []
+                        )
+                        session.add(chatlog)
+                        session.commit()
                 return redirect(url_for('chat.chat_list'))
             
             case 'join_submit':
                 with Session(engine) as session, session.begin():
                     chatlog = session.execute(select(ChatLog).filter_by(id=request.form['room_id'])).scalar()
                     chatlog = chatlog.__dict__
-                # print(chatlog)
-                return redirect(url_for("chat.chat", room_id=chatlog["id"]), code=307)
+                print(chatlog)
+                return redirect(url_for("chat.chat", room_id=chatlog["id"]))
             
         
